6d6f82db38782f794f2778d68191bcb29a98b21e
LiliiaKlymenko <liliia.klymenko@hneu.net>
2015-10-13 20:02:25 +0300

    3

--- TestProject/DownloadFile.cs
+++ TestProject/DownloadFile.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Net;
+using System.IO;
 
 namespace TestProject
 {
@@ -12,11 +13,18 @@ namespace TestProject
 		{
 			using (WebClient webClient = new WebClient())
 			{
-				Console.WriteLine("Downloading File \"{0}\" from \"{1}\" .......\n", fileName, address);
-				webClient.DownloadFile(address, @fileName);
-				Console.WriteLine("Successfully Downloaded File \"{0}\" from \"{1}\"", fileName, address);
-				string appDirectory = Environment.CurrentDirectory;
-				Console.WriteLine("\nThe file is saved at:\t" + fileName);               
+                if (File.Exists(fileName))
+                {
+                    Console.WriteLine("File already exist!");
+                }
+                else
+                {
+                    Console.WriteLine("Downloading File \"{0}\" from \"{1}\" .......\n", fileName, address);
+                    webClient.DownloadFile(address, @fileName);
+                    Console.WriteLine("Successfully Downloaded File \"{0}\" from \"{1}\"", fileName, address);
+                    string appDirectory = Environment.CurrentDirectory;
+                    Console.WriteLine("\nThe file is saved at:\t" + fileName);
+                }
 			}
 
 		}
